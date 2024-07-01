16d57b469a50c383700df8cb5b9ce3d6a1b653e5
johnntourmas <giannisntour2000@gmail.com>
2024-07-02 00:49:04 +0300

    Remove useless files and add improvements in azure implementation

--- src/main/java/com/unipi/cloudpatterns/controller/HomeController.java
+++ src/main/java/com/unipi/cloudpatterns/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.unipi.cloudpatterns.controller;
 import com.unipi.cloudpatterns.service.AzureBlobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,10 +17,17 @@ public class HomeController {
         this.azureBlobService = azureBlobService;
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = {"/", "/{path:[^\\.]*}"})
     public String redirect() {
         return "forward:/index.html";
         // return "redirect:" + azureBlobService.getBlobUrl("index.html");
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/static/{folder}/{filename:.+}")
+    public String getStaticFile(@PathVariable String folder, @PathVariable String filename) {
+        return "redirect:" + azureBlobService.getBlobUrl("static/" + folder + "/" + filename);
     }
 }
 
